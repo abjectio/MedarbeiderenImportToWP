6cfa15aa87c44367d8adbd2c4f906cdec0599f04
abjectio <knut.erik@unlike.no>
2016-03-22 00:56:43 +0100

    added bootstrap

--- MedarbeiderenImportToWP.php
+++ MedarbeiderenImportToWP.php
@@ -44,6 +44,11 @@ License: GPLv3
 	}	
 	
 	function myplugin_settings_admininit() {
+?>	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<?php	
 		//register_setting( 'myplugin', 'myplugin_setting_1', 'intval' );
 		//register_setting( 'myplugin', 'myplugin_setting_2', 'intval' );
 	}
--- medarbeideren-import.php
+++ medarbeideren-import.php
@@ -15,17 +15,26 @@ function showHTML() {
 	$refresh_btn = $_REQUEST['refresh-log-btn'];
 	$import_btn = $_REQUEST['start-import-btn'];
 	
-	$title = __('Medarbeideren import');
 	?>
-		<h1><?php echo esc_html( $title ); ?></h1>
 
-		<hr>
+ </br>
+ <div class="row">
+  <div class="col-sm-6">
+	<div class="panel panel-info">
+	  <div class="panel-heading">Medarbeideren import - informasjon</div>
+	  <div class="panel-body">
+	
 		<p>Funksjonen nedenfor eksekverer en import rutine på tjeneren. Den vil hente data fra Medarbeideren og importere disse inn i wordpress instansen.</p>
 		<p>Når importen er startet vil du ikke få noe øyeblikkelig tilbakemelding om den er ferdig eller fortsatt kjørende, da eksekveringen går på tjeneren uavhengig av denne siden (en prosess i bakgrunnen startes på tjeneren).</p>
 		<p>For å ha en noenlunde feeling på hvor prosessen er kan du trykke på <b>Refresh Log</b> knappen nedenfor, som vil vise de siste 20 linjene fra import loggen.</p>
 		<p style="color:red;">NB: Ikke eksekver mange importer samtidig, dette er ennå ikke testet! Ha tålmodighet og se om importen er ferdig først.</p>
 
-		<hr>
+	  </div> <!-- Panel body -->
+	</div> <!-- Panel info import -->
+
+	<div class="panel panel-default">
+	  <div class="panel-heading">Import</div>
+	  <div class="panel-body">
 
 		<form method="post" action="admin.php?page=medarbeideren-import">
 			<label >Velg gruppe for import : </label>
@@ -36,26 +45,41 @@ function showHTML() {
 			  <option value="konfirmant" <?php echo ($chosen=='konfirmant' ? 'selected' : '') ?>>Konfirmant</option>
 			  <option value="tabago" <?php echo ($chosen=='tabago' ? 'selected' : '') ?>>Tabago</option>
 			</select>
-			
-			<p>
-			  <button name="start-import-btn" class="button-primary">Start Import</button>			  
-			</p>
-			<hr>
+			</br>
+			<button type="submit" name="start-import-btn" class="btn btn-danger">Start import</button>
 		</form>
+	  </div> <!-- Panel body -->
+	</div> <!-- Panel info default -->
+
+	<div class="panel panel-default">
+	  <div class="panel-heading">Les fra server loggen</div>
+	  <div class="panel-body">
 
 			<p>Oppfrisker loggen med et utdrag fra importen (resultat vises nedenfor knappen).</p>
 			<p>Når importen er ferdig vil du se teksten <b>[END IMPORT]</b> i loggen.</p>
 			<form action="admin.php?page=medarbeideren-import" method="post">
-				<button name="refresh-log-btn" class="button-secondary">Refresh Log</button>
+				<button name="refresh-log-btn" class="btn btn-primary">Refresh Log</button>
 			</form>	
-			
-			<hr><p>OUTPUT:</p>
+	  </div> <!-- Panel body -->
+	</div> <!-- Panel info default -->
+ </div> <!-- venstre kolonne -->
+ <div class="col-sm-6">
+	<div class="panel panel-primary">
+	  <div class="panel-heading">Server logg</div>
+	  <div class="panel-body">
 			
 			<?php 
 			
 				//Check what action to perform - refresh or the import?
 				if(isset($chosen) && isset($import_btn)) {importEvents($chosen);};
 				if(isset($refresh_btn)){refreshLog();};	
+				
+			?>
+	</div> <!-- Panel body -->
+	</div> <!-- Panel info default -->
+ </div> <!-- hoyre kolonne -->
+</div>	 <!-- grid end -->
+<?php	
 }
 
 
